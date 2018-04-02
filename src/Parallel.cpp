a95d2a4f754cf0cb78b34b828ffb4b0c21a1ee71
Tasos Bakogiannis <abk@cslab.ece.ntua.gr>
2018-04-02 18:28:48 +0300

    in the process

--- src/Parallel.cpp
+++ src/Parallel.cpp
@@ -7,7 +7,7 @@ uint64_t calcParallelSum(IteratorPair valIter)
     const uint64_t *values = &(*valIter.first);
     size_t size = valIter.second - valIter.first;
 
-    ParrallelSum s(values);
+    ParallelSum s(values);
 
     tbb::parallel_reduce(tbb::blocked_range<size_t>(0, size), s);
 
