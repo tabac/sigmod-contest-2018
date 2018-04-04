daaa0e842c052f097f68771bfd02ed3a9c8e7d3b
Tasos Bakogiannis <t.bakogiannis@gmail.com>
2018-04-04 11:43:29 +0300

    stick with that one.

--- include/Mixins.hpp
+++ include/Mixins.hpp
@@ -27,8 +27,8 @@ using IteratorDoublePair = std::pair<std::vector<uint64Pair>::const_iterator,
 //---------------------------------------------------------------------------
 static const bool INDEXES_ON = true;
 static const bool INDEXES_CREATE_ON_MERGE = true;
-static const size_t PAIRS_GRAIN_SIZE = 512;
-static const size_t SINGLES_GRAIN_SIZE = 1024;
+static const size_t PAIRS_GRAIN_SIZE = 256;
+static const size_t SINGLES_GRAIN_SIZE = 512;
 //---------------------------------------------------------------------------
 class DataReaderMixin {
     // TODO: Would be nice not to pass `filterInfo` and then ignore it...
